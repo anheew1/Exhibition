0f05a5627577776f292aca290bbca662ce1030a0
anheew1 <anheew1@kookmin.ac.kr>
2019-06-05 00:03:46 +0900

    add com.roughike:bottom-bar

--- app/src/main/java/kr/ac/anheew1kookmin/exhibition/MainActivity.java
+++ app/src/main/java/kr/ac/anheew1kookmin/exhibition/MainActivity.java
@@ -1,7 +1,11 @@
 package kr.ac.anheew1kookmin.exhibition;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artwork);
+
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_artwork) {
+
+                }
+                if (tabId == R.id.tab_place) {
+
+                }
+                if (tabId == R.id.tab_upload) {
+
+                }
+                if (tabId == R.id.tab_message) {
+
+                }
+                if (tabId == R.id.tab_mypage) {
+
+                }
+
+            }
+        });
     }
 }
