b53de3c7e544e863972daf6333fc4d89895bccce
anheew1 <anheew1@kookmin.ac.kr>
2019-06-17 17:27:56 +0900

    complete login and  register

--- app/src/main/java/kr/ac/anheew1kookmin/exhibition/LoginActivity.java
+++ app/src/main/java/kr/ac/anheew1kookmin/exhibition/LoginActivity.java
@@ -2,17 +2,28 @@ package kr.ac.anheew1kookmin.exhibition;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText edit_id;
     private EditText edit_pass;
     private Button btn_login;
     private Button btn_register;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +32,25 @@ public class LoginActivity extends AppCompatActivity {
         edit_id = (EditText) findViewById(R.id.editText_login_ID);
         edit_pass = (EditText) findViewById(R.id.editText_login_pass);
 
+        btn_login = (Button) findViewById(R.id.btn_login_submit);
+        btn_register = (Button) findViewById(R.id.btn_login_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String id = edit_id.getText().toString();
-                String pass = edit_id.getText().toString();
-                //todo
+                String pass = edit_pass.getText().toString();
+
+
+                if( id.equals("")  || pass.equals("")){
+                    Toast toast = Toast.makeText(getApplicationContext(),"All blank field must filled",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else {
+                    login(id,pass);
+                }
             }
         });
         btn_register.setOnClickListener(new View.OnClickListener() {
@@ -36,5 +60,23 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void login(String id,String pass){
+        mAuth.signInWithEmailAndPassword(id,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Log.w("fail", "signInWithEmail:failure", task.getException());
+                    Toast.makeText(getApplicationContext(), "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/kr/ac/anheew1kookmin/exhibition/RegisterActivity.java
+++ app/src/main/java/kr/ac/anheew1kookmin/exhibition/RegisterActivity.java
@@ -2,12 +2,20 @@ package kr.ac.anheew1kookmin.exhibition;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText edit_id;
@@ -16,34 +24,58 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText edit_confirmPass;
 
     private Button btn_register;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         edit_id = (EditText) findViewById(R.id.editText_register_ID);
         edit_name = (EditText) findViewById(R.id.editText_register_name) ;
         edit_pass = (EditText) findViewById(R.id.editText_register_pass);
         edit_confirmPass = (EditText) findViewById(R.id.editText_register_confirmPass);
 
+        btn_register = (Button) findViewById(R.id.btn_register_submit);
+        mAuth = FirebaseAuth.getInstance();
+
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(edit_pass.getText().toString().equals(edit_confirmPass.toString())){
+                if(!(edit_pass.getText().toString().equals(edit_confirmPass.getText().toString()))){
                     Toast toast = Toast.makeText(getApplicationContext(),"Passwords are not equal",Toast.LENGTH_SHORT);
                     toast.show();
                 }
-                else if(edit_id.toString() == "" || edit_name.toString() == "" || edit_pass.toString() == ""
-                        ||edit_confirmPass.toString()==""){
+                else if(edit_pass.getText().toString().length() <6){
+                    Toast.makeText(getApplicationContext(),"Passwords must set over 6",Toast.LENGTH_SHORT);
+                }
+                else if(edit_id.getText().toString().equals("") || edit_name.getText().toString().equals("")
+                        || edit_pass.getText().toString().equals("")
+                        ||edit_confirmPass.getText().toString().equals("")){
                     Toast toast = Toast.makeText(getApplicationContext(),"All blank field must filled",Toast.LENGTH_SHORT);
                     toast.show();
                 }
                 else{
-                    //register!
-                    Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                    register(edit_id.getText().toString(),edit_pass.getText().toString());
+                    Log.d("Login","w");
+                }
+            }
+        });
+    }
+    private void register(String id , String pass){
+        mAuth.createUserWithEmailAndPassword(id,pass).addOnCompleteListener(
+                this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Log.d("ad","Succes");
+                    Toast.makeText(getApplicationContext(), "Register Complete", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                     startActivity(intent);
                 }
-
+                else{
+                    Log.w("not","fail",task.getException());
+                    Toast.makeText(getApplicationContext(), "Register Failure", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
