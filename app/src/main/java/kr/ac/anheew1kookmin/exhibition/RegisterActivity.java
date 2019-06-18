849d1cd23228f4be282eed8a725b75f497b9aa48
anheew1 <anheew1@kookmin.ac.kr>
2019-06-18 13:32:11 +0900

    Register update user db

--- app/src/main/java/kr/ac/anheew1kookmin/exhibition/RegisterActivity.java
+++ app/src/main/java/kr/ac/anheew1kookmin/exhibition/RegisterActivity.java
@@ -16,6 +16,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+import kr.ac.anheew1kookmin.exhibition.Entity.User;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText edit_id;
@@ -68,6 +73,7 @@ public class RegisterActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()) {
                     Log.d("ad","Succes");
+                    registerToFirebaseDB();
                     Toast.makeText(getApplicationContext(), "Register Complete", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                     startActivity(intent);
@@ -78,5 +84,15 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    private void registerToFirebaseDB(){
+        String id = edit_id.getText().toString();
+        String name = edit_name.getText().toString();
+        // id is email
+        User user = new User("uid",id,name);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        String uid = ref.child("/User").push().getKey();
+        user.setUid(uid);
+        ref.child("/User").child(uid).setValue(user);
     }
 }
